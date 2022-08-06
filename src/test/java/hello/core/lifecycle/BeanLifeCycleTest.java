d231746b93ecf84b54daf1db15b67366770833fc
YoonDaHee <70131760+sayurrdahh@users.noreply.github.com>
2022-08-06 19:27:18 +0900

    빈 등록 초기화, 소멸 메소드

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,12 +18,16 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
         }
     }
+
+    //메서드 이름을 자요롭게 쓸 수 있다
+    //스프링 빈이 스픵 코드에 의존하지 않는다
+    //코드가 아니라 설정 정보를 사용하기 때문에 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 종료 메서드를 적용할 수 있다.
 
 }
